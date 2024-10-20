14cc0aaeaf93cdeef9736014c992a8b5f198ffa5
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-10-20 21:43:13 +0300

    SqlServer configurations were added.

--- src/salesTrackingManagementAPI/Persistence/PersistenceServiceRegistration.cs
+++ src/salesTrackingManagementAPI/Persistence/PersistenceServiceRegistration.cs
@@ -12,7 +12,7 @@ public static class PersistenceServiceRegistration
 {
     public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("BaseDb"));
+        services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("BaseDb")));
         services.AddDbMigrationApplier(buildServices => buildServices.GetRequiredService<BaseDbContext>());
 
         services.AddScoped<IEmailAuthenticatorRepository, EmailAuthenticatorRepository>();
